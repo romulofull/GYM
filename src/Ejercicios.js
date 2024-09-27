7350295ee988fb2ce75a45dca9f08b6ba940c1ee
romulofull <romulo.castro@gmail.com>
2024-09-26 19:51:20 -0500

    segunda version

--- src/Ejercicios.js
+++ src/Ejercicios.js
@@ -2,22 +2,23 @@ import React from 'react';
 import gallery from './assets/gallery';  
 
 const Ejercicios = ({ bodyPart }) => {
- 
   const images = {
-    pecho: gallery.imagen1,
-    espalda: gallery.imagen2,
-    brazos: gallery.imagen3,
-    antebrazo: gallery.imagen4,
-    hombros: gallery.imagen5,
-    abdominales: gallery.imagen6,
-    gluteos: gallery.imagen7,
-    piernas: gallery.imagen8,
+    pecho: [gallery.imagen1, gallery.imagen2, gallery.imagen3],
+    espalda: [gallery.imagen4, gallery.imagen5],
+    brazos: [gallery.imagen6, gallery.imagen7, gallery.imagen8],
+    antebrazo: [gallery.imagen9, gallery.imagen10],
+    hombros: [gallery.imagen11, gallery.imagen12, gallery.imagen13],
+    abdominales: [gallery.imagen14, gallery.imagen15],
+    gluteos: [gallery.imagen18, gallery.imagen19],
+    piernas: [gallery.imagen20, gallery.imagen21, gallery.imagen22],
   };
 
   return (
     <div className="chart-container">
       {images[bodyPart] ? (
-        <img src={images[bodyPart]} alt={`${bodyPart} ejercicios`} style={{ width: '100%', height: 'auto' }} />
+        images[bodyPart].map((img, index) => (
+          <img key={index} src={img} alt={`${bodyPart} ejercicio ${index + 1}`} />
+        ))
       ) : (
         <p>Selecciona una parte del cuerpo para ver los ejercicios</p>
       )}
--- src/assets/gallery.js
+++ src/assets/gallery.js
@@ -6,6 +6,18 @@ import foto5 from './imagenes/foto5.gif';
 import foto6 from './imagenes/foto6.gif';
 import foto7 from './imagenes/foto7.gif';
 import foto8 from './imagenes/foto8.gif';
+import foto9 from './imagenes/foto9.gif';
+import foto10 from './imagenes/foto10.gif';
+import foto11 from './imagenes/foto11.gif';
+import foto12 from './imagenes/foto12.gif';
+import foto13 from './imagenes/foto13.gif';
+import foto14 from './imagenes/foto14.gif';
+import foto15 from './imagenes/foto15.gif';
+import foto18 from './imagenes/foto18.gif';
+import foto19 from './imagenes/foto19.gif';
+import foto20 from './imagenes/foto20.gif';
+import foto21 from './imagenes/foto21.gif';
+import foto22 from './imagenes/foto22.gif';
 
 const gallery = {
 imagen1: foto1,
@@ -16,5 +28,17 @@ imagen5: foto5,
 imagen6: foto6,
 imagen7: foto7,
 imagen8: foto8,
+imagen9: foto9,
+imagen10: foto10,
+imagen11: foto11,
+imagen12: foto12,
+imagen13: foto13,
+imagen14: foto14,
+imagen15: foto15,
+imagen18: foto18,
+imagen19: foto19,
+imagen20: foto20,
+imagen21: foto21,
+imagen22: foto22,
 };
 export default gallery;
